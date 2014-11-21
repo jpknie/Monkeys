3907da675171fdfeb4ba307d7f30b308ecc66ce3
Jani Nieminen <jpkniem@gmail.com>
2014-11-21 07:45:11 +0200

    Introduce test configuration

--- monkeysApp/settings.py
+++ monkeysApp/settings.py
@@ -1,16 +1,21 @@
 import os
 
+
 class Config(object):
-	SECRET_KEY = "this is very secret key"
-	APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
-	PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
+    SECRET_KEY = "this is very secret key"
+    APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
+    PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
+
 
 class ProductionConfig(Config):
-	ENV = 'production'
-	DEBUG = False
-	SQLALCHEMY_DATABASE_URI = 'postgresql://janiniem:devpass@localhost/monkeys'
+    ENV = 'production'
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql://janiniem:devpass@localhost/monkeys'
+
 
 class DevelopmentConfig(Config):
-	SQLALCHEMY_DATABASE_URI = 'postgresql://janiniem:devpass@localhost/monkeys_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://janiniem:devpass@localhost/monkeys_dev'
 
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://janiniem:devpass@localhost/monkeys_test'
