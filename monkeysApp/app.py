cad838256e45225274d10f9fbe65b1f4a92f9dd1
Jani Nieminen <jpkniem@gmail.com>
2014-11-24 01:01:01 +0200

    Register blueprints in app factory

--- monkeysApp/app.py
+++ monkeysApp/app.py
@@ -4,11 +4,17 @@ from flask import Flask, _app_ctx_stack
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, Query as SAQuery
 
+import monkeysApp.views.main
 
 DbSession = scoped_session(sessionmaker(),
                            # __ident_func__ should be hashable, therefore used
                            # for recognizing different incoming requests
                            scopefunc=_app_ctx_stack.__ident_func__)
+
+
+def register_blueprints(app):
+    app.register_blueprint(monkeysApp.views.main.main_blueprint)
+    return None
 
 
 def create_app(name, config_obj):
@@ -17,5 +23,5 @@ def create_app(name, config_obj):
 
     app.engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 
+    register_blueprints(app)
     return app
-
