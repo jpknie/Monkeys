89e0d57b41d487bda332850a7134dc4f0e7b9081
Jani Nieminen <jpkniem@gmail.com>
2014-11-24 00:53:20 +0200

    add convenient command to list the routes of application

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 import subprocess
 
-
+from flask import url_for
 from flask.ext.script import Manager, Server
 
 from monkeysApp.app import create_app
@@ -23,6 +23,23 @@ def test():
     status = subprocess.call("py.test", shell=True)
     sys.exit(status)
 
+@manager.command
+def list_routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print line
 
 @manager.command
 def createdb():
