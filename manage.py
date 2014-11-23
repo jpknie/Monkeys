e16b9052ca8e4ba6a122fc755e37f767627fa08a
Jani Nieminen <jpkniem@gmail.com>
2014-11-23 15:05:32 +0200

    import Base in management script

--- manage.py
+++ manage.py
@@ -5,22 +5,25 @@ import os
 from flask.ext.script import Manager, Server
 
 from monkeysApp.app import create_app
+from monkeysApp.models import Base
 from monkeysApp.settings import DevelopmentConfig, ProductionConfig
 
 if os.environ.get('MONKEYAPP_ENV') == 'production':
-	app = create_app(__name__, ProductionConfig)
+    app = create_app(__name__, ProductionConfig)
 else:
-	app = create_app(__name__, DevelopmentConfig)
+    app = create_app(__name__, DevelopmentConfig)
 
 manager = Manager(app)
 
+
 @manager.command
 def createdb():
-	Base.metadata.create_all(bind=app.engine)
+    Base.metadata.create_all(bind=app.engine)
+
 
 manager.add_command("run", Server())
 
 if __name__ == '__main__':
-	manager.run()
+    manager.run()
 
 
