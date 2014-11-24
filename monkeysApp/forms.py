41cdb19183f84a594b5b1d19c20b68b6487778a2
Jani Nieminen <jpkniem@gmail.com>
2014-11-24 02:03:06 +0200

    Forms

---
+++ monkeysApp/forms.py
@@ -0,0 +1,16 @@
+__author__ = 'janiniem'
+
+from flask.ext.wtf import Form
+from wtforms import TextField, PasswordField, IntegerField
+from wtforms.validators import Required, Email
+
+
+class LoginForm(Form):
+    login_name = TextField('Login', [Required()])
+    password = PasswordField('Password', [Required()])
+
+
+class MonkeyForm(Form):
+    email = TextField('Email', validators=[Required(), Email(message=u'Invalid email address.')])
+    name = TextField('Name', validators=[Required()])
+    age = IntegerField('Age', validators=[Required()])
