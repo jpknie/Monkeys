9c26475a603b1044d6303e0a510bfab786a1498f
Jani Nieminen <jpkniem@gmail.com>
2014-11-24 22:42:58 +0200

    Test monkey_add view

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,8 +1,24 @@
 __author__ = 'janiniem'
+from monkeysApp.models import Monkey
 
 
 def test_index(test_client):
     with test_client as client:
         rv = client.get('/')
     assert "Hello world!" in rv.data
+
+
+def test_monkey_add(app, test_client, session):
+    data = dict(name="Test Monkey",
+                email="test@monkey.com",
+                age=12)
+
+    with test_client as client:
+        rv = client.post('/monkeys/add', data=data, follow_redirects=True)
+        assert rv.status_code is 200
+        test_monkey = session.query(Monkey).first()
+
+        assert test_monkey.name == "Test Monkey"
+        assert test_monkey.email == "test@monkey.com"
+        assert test_monkey.age == 12
 
