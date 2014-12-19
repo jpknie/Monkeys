a77850d73b5b829df7c41601719bfcce73513ef6
Jani Nieminen <jpkniem@gmail.com>
2014-12-19 17:13:01 +0200

    Move login_required decorator after the route definition. Fix user addition and update logic. Index will list all the users. Do not allow other than admin to add new users.

--- monkeysApp/views/main.py
+++ monkeysApp/views/main.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Blueprint, g, render_template, request, flash, redirect, url_for, session, make_response
 from flask.ext.login import current_user, login_required, logout_user
 
@@ -21,23 +23,33 @@ def before_request():
 
 
 @main_blueprint.route('/', methods=['GET'])
+@login_required
 def index():
     """Browse user profiles"""
     page_title = "Browse profiles"
-    return render_template('monkeys.html', page_title=page_title)
+    monkeys = Monkey.query.all()
+    return render_template('monkeys.html', page_title=page_title, profiles=monkeys)
 
 
 @main_blueprint.route('/monkeys/add', methods=['GET', 'POST'])
+@login_required
 def monkey_add():
     """
     Add new monkey to database
     """
+    if g.user.is_admin() != 1:
+        flash('You are not allowed in here!')
+        return redirect(url_for('main_blueprint.index'))
 
     form = MonkeyForm(request.form)
     page_title = "Add monkey"
 
+    random_face = ['trollface.png', '9.jpg', 'monkey.png']
+
     if request.method == 'POST' and form.validate_on_submit():
         monkey = Monkey()
+        face_num = random.randint(0, len(random_face) - 1)
+        monkey.face_image = random_face[face_num]
         form.populate_obj(monkey)
         try:
             db.add(monkey)
@@ -51,11 +63,11 @@ def monkey_add():
                 return redirect(url_for('main_blueprint.monkey_add'))
         flash('New monkey added successfully')
         return redirect(url_for('main_blueprint.monkey_add'))
-    return render_template('monkey_form.html', form_action='/monkeys/add', form=form, page_title=page_title)
+    return render_template('monkey_form.html', form_action='/monkeys/add', form=form, page_title=page_title, form_edit=False)
 
 
-@login_required
 @main_blueprint.route('/monkeys/friends', methods=['GET'])
+@login_required
 def monkey_friends():
     """Show list of friends of g.user"""
     page_title = "Your friends"
@@ -64,8 +76,8 @@ def monkey_friends():
     return render_template('monkey_friends.html', friends=friends, page_title=page_title)
 
 
-@login_required
 @main_blueprint.route('/monkeys/friend_requests', methods=['GET'])
+@login_required
 def monkey_friend_requests():
     """Get the list of pending friend requests"""
     page_title = "Friend requests"
@@ -74,35 +86,26 @@ def monkey_friend_requests():
     return render_template('friend_requests.html', page_title=page_title, friend_requests=list(frqs))
 
 
-@login_required
 @main_blueprint.route('/monkeys/profile', methods=['GET', 'POST'])
+@login_required
 def monkey_my_profile():
     """Get the profile of logged in user"""
     page_title = "Your profile"
     me = g.user
+
     form = MonkeyForm(request.form, obj=me)
     if form.validate_on_submit() and request.method == 'POST':
-        monkey = Monkey()
+        monkey = Monkey.query.get(me.id)
         form.populate_obj(monkey)
-        try:
-            db.add(monkey)
-            db.commit()
-
-        except IntegrityError, exc:
-            reason = exc.message
-            if reason.find('unique constraint'):
-                flash('Email already in use, please use another email address!')
-                db.rollback()
-                return redirect(url_for('main_blueprint.monkey_my_profile'))
-        flash('New monkey added successfully')
+        db.commit()
         return redirect(url_for('main_blueprint.monkey_my_profile'))
 
         return render_template('monkey_form.html', page_title=page_title, form=form, my_profile=True, monkey=me)
-    return render_template('monkey_form.html', page_title=page_title, form=form, my_profile=True, monkey=me)
+    return render_template('monkey_form.html', page_title=page_title, form=form, my_profile=True, monkey=me, form_edit=True)
 
 
-@login_required
 @main_blueprint.route('/monkeys/profile/<int:monkey_id>', methods=['GET'])
+@login_required
 def monkey_show_profile(monkey_id):
     """Show profile with monkey id"""
     me = g.user
@@ -120,13 +123,13 @@ def monkey_show_profile(monkey_id):
                                page_title=monkey.name,
                                my_profile=my_profile,
                                is_my_friend=is_my_friend,
-                               friend=monkey,
+                               profile=monkey,
                                sent_friend_request=sent_friend_request,
                                rcvd_friend_request=rcvd_friend_request)
 
 
-@login_required
 @main_blueprint.route('/monkeys/remove_friend/<int:monkey_id>', methods=['GET'])
+@login_required
 def remove_friend(monkey_id):
     """Unfriend monkey"""
     me = g.user
@@ -139,8 +142,8 @@ def remove_friend(monkey_id):
         return make_response("", 400)
 
 
-@login_required
 @main_blueprint.route('/monkeys/make_friend/<int:monkey_id>', methods=['GET'])
+@login_required
 def make_friend(monkey_id):
     """Be friends with monkey"""
     send_to = Monkey.query.filter_by(id=monkey_id).first()
@@ -171,8 +174,8 @@ def login():
     return render_template('login.html', form=form)
 
 
-@login_required
 @main_blueprint.route('/logout')
+@login_required
 def logout():
     """Logout currently authenticated user"""
     if g.user is not None:
